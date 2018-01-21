b8976945e43185129575871d7fd355f632cf230f
Alex Edwards <32015939+youtweakit@users.noreply.github.com>
2018-01-21 17:09:38 +0300

    Update RegisterAddressPage.js

--- web-dapp/src/components/RegisterAddressPage.js
+++ web-dapp/src/components/RegisterAddressPage.js
@@ -10,7 +10,7 @@ class RegisterAddressPage extends Component {
             country: 'US',
             city: '',
            
-           */ state&zip&adress removed?/*
+         
             loading: false,
         };
     }
