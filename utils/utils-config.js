30fbe2e273dfc51ab48c21164b766e2d55bf080e
Alex Edwards <32015939+youtweakit@users.noreply.github.com>
2018-01-21 18:57:31 +0300

    Update utils-config.js

--- utils/utils-config.js
+++ utils/utils-config.js
@@ -9,7 +9,7 @@ var utils_config = {
     owner: '$TEST',
     //owner: '0x00a329c0648769a73afac7f9381e08fb43dbea72',
     source_file: path.join(__dirname, '../contract/Main.sol'),
-    contract_name: 'YTIhackdapp.sol',
+    contract_name: 'YTIhackdapp',
     network: network,
     contract_output: path.join(__dirname, '../web-dapp/src/contract-output.json'),
 };
