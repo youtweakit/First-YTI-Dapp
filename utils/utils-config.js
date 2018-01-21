06f59b347dc41f3a5b518e9c77ff61fc3e3c164c
Alex Edwards <32015939+youtweakit@users.noreply.github.com>
2018-01-21 18:53:57 +0300

    local rpc ch to sokol

--- utils/utils-config.js
+++ utils/utils-config.js
@@ -3,13 +3,13 @@ var Web3 = require('web3');
 
 //var server_config = require('../web-dapp/server-config');
 
-var rpc = 'http://localhost:8545';
+var rpc = 'https://sokol.poa.network';
 var network = new Web3.providers.HttpProvider(rpc);
 var utils_config = {
     owner: '$TEST',
     //owner: '0x00a329c0648769a73afac7f9381e08fb43dbea72',
     source_file: path.join(__dirname, '../contract/Main.sol'),
-    contract_name: 'ProofOfPhysicalAddress',
+    contract_name: 'YTIhackdapp.sol',
     network: network,
     contract_output: path.join(__dirname, '../web-dapp/src/contract-output.json'),
 };
