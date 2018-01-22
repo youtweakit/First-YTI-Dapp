f975e3390034a0da3c8cb4a81aa5371d1d05c59b
Alex Edwards <32015939+youtweakit@users.noreply.github.com>
2018-01-22 10:59:48 +0300

    mailgun key

--- web-dapp/src/server-config-private.js
+++ web-dapp/src/server-config-private.js
@@ -2,7 +2,7 @@
 
 module.exports = function (cfg_public) {
     return {
-        lob_api_key: '*** LOB TEST OR PROD API KEY ***',
+        lob_api_key: 'key-bd2541ea423cc66764f19ca6bc1d27b8',
         rpc: 'https://sokol.poa.network',
         signer: '0x2c7241672be5032f3f8227D6A8B0FFE04088EEEB', // with 0x prefix
         signer_private_key: 'd194bb80c905679ad8033adfd4c7f634854050e32dc2fd73c9beff484b62571d', // without 0x prefix
